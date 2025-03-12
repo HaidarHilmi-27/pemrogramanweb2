5b043ea8e9fb6f9800a4525d40ec78b678220637
Haidar-Hilmi27 <haidar.hilmi2727@gmail.com>
2025-03-12 07:57:30 +0700

    pertemuan 3

---
+++ pertemuan3/praktikum/admin/panggil.php
@@ -0,0 +1,19 @@
+<?php 
+    include_once 'top.php';
+?>
+<div id="page-content-wrapper">
+                <!-- Top navigation-->
+    <?php 
+      include_once 'menu.php';
+    ?>
+                <!-- Page content-->
+    <div class="container-fluid">
+        <h1 class="mt-4">Selamat Datang</h1>
+    <?php
+        include_once '../../tes.html';
+    ?>
+    </div>
+</div>
+<?php 
+    include_once 'bottom.php';
+?>
